8c55b88006eb549c01a419835b552184b24fc091
akierans <akierans@gmail.com>
2021-01-19 18:28:16 +0000

    Added Mongo DB and an API Call to DB for userDetails

--- 05_mongodb/app.js
+++ 05_mongodb/app.js
@@ -1,12 +1,23 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 import user from './routes/user';
-let app = express();
+import {MongoClient} from 'mongodb';
 
+const CONN_URL = 'mongodb://localhost:27017';
+let mongoClient = null;
+MongoClient.connect(CONN_URL, { useNewUrlParser: true }, function (err, client) {
+	mongoClient = client;
+})
+
+let app = express();
 //parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 //parse application/json
 app.use(bodyParser.json())
+app.use((req,res,next)=>{
+	req.db = mongoClient.db('test');
+	next();
+})
 
 app.get('/',(req,res,next)=>{
    res.status(200).send({
@@ -19,4 +30,14 @@ app.use('/user',user);
 app.listen(30006,()=>{
    console.log(' ********** : running on 30006');
 })
+
+process.on('exit', (code) => {
+	mongoClient.close();
+	console.log(`About to exit with code: ${code}`);
+});
+process.on('SIGNT', function() {
+	console.log("Caught interrupt signal");
+	process.exit();
+});
+
 module.exports = app;
--- 05_mongodb/routes/user.js
+++ 05_mongodb/routes/user.js
@@ -2,13 +2,13 @@ import {Router} from 'express';
 
 let router = Router();
 
-router.post('/hello',(req,res)=>{
-	let forename = req.body.forename;
-	let surname = req.body.surname;
-	res.status(200).send({
-		status:true,
-		response:`Hello ${forename} ${surname}`
-	});
+import {getUserDetails} from '../services/UserService';
+router.post('/hello',async (req,res)=>{
+   let uname = req.body.username;
+   let userDetails = await getUserDetails(req.db,uname)
+   res.status(200).send({
+      status:true,
+      response:userDetails
+   });
 });
-
 export default router;
